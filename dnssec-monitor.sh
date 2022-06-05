2f125b8ffb4e723385852f63c810e99ddc1efa81
Tom Shaw <tom@flipkick.media>
2022-06-05 13:49:36 +0100

    updated pid handling for tail

--- dnssec-monitor.sh
+++ dnssec-monitor.sh
@@ -54,8 +54,6 @@ ip a a 10.0.254.1 dev eno1
 
 trap "trap_exit" SIGINT SIGKILL SIGSTOP 15
 
-logger "flags: ${LOGGER_FLAGS}"
-
 LOGGER_FLAGS=${LOGGER_FLAGS} ${DIR}/dnssec-monitor.sh --clean &
 monitor_pid=$!
 logger "monitor running on ${monitor_pid}"
@@ -63,9 +61,14 @@ logger "monitor running on ${monitor_pid}"
 # main monitoring/update
 
 files=$(find ${BIND_LOG_PATH} -type f -not -name zone_transfers -not -name queries)
+logger "flags: ${LOGGER_FLAGS}"
 logger "monitoring $files for CDS updates"
 
-tail -n0 -f $files | stdbuf -oL grep '.*' |
+#tail -n0 -f $files | stdbuf -oL grep '.*' |
+(
+  tail -n0 -f $files &
+  echo $! >&3
+) 3>tail_pid | stdbuf -oL grep '.*' |
   while IFS= read -r line; do
     #line='04-Jun-2022 07:12:02.164 dnssec: info: DNSKEY node.flipkick.media/ECDSAP384SHA384/29885 (KSK) is now published'
     if grep -P '.*info: DNSKEY.*\(KSK\).*published.*' <<<"$line"; then
@@ -87,5 +90,5 @@ tail -n0 -f $files | stdbuf -oL grep '.*' |
       fi
     fi
   done &
-tail_pid=$!
+tail_pid=$(<pid)
 wait $tail_pid
