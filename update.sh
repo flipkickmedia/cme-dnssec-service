5bc56056db9217c21bfada4f43a94ad3738a5b1e
Tom Shaw <tom@flipkick.media>
2022-06-12 21:14:00 +0100

    added location for nsup script

--- update.sh
+++ update.sh
@@ -37,13 +37,13 @@ for view in ${views[@]}; do
 
   dig -b ${ip_addr} "@${NS_SERVER}" +norecurse "${DOMAIN}". DNSKEY | dnssec-dsfromkey -a SHA-384 -f - "${DOMAIN}" | tee "/tmp/cme-dnssec-monitor/dsset-${DOMAIN}." >/dev/null
   dig -b ${ip_addr} "@${NS_SERVER}" +dnssec +noall +answer "${DOMAIN}" DNSKEY "${DOMAIN}" CDNSKEY "${DOMAIN}" CDS | tee "/tmp/cme-dnssec-monitor/file-${DOMAIN}" >/dev/null
-  dnssec-cds -a SHA-384 -s-86400 -T "${TTL}" -u -i -f "/tmp/cme-dnssec-monitor/file-${DOMAIN}" -d . -i.orig "${DOMAIN}" | tee ./nsup >/dev/null
+  dnssec-cds -a SHA-384 -s-86400 -T "${TTL}" -u -i -f "/tmp/cme-dnssec-monitor/file-${DOMAIN}" -d . -i.orig "${DOMAIN}" | tee "/tmp/cme-dnssec-monitor/nsup" >/dev/null
 
   if [[ $CME_DNSSEC_MONITOR_DEBUG -eq 1 ]]; then
     cat <<EOF
 server ${NS_SERVER}
 zone ${DOMAIN}
-$(cat ./nsup)
+$(cat /tmp/cme-dnssec-monitor/nsup)
 send
 EOF
   fi
@@ -52,11 +52,11 @@ EOF
     cat <<EOF
 server ${NS_SERVER}
 zone ${DOMAIN}
-$(cat ./nsup)
+$(cat /tmp/cme-dnssec-monitor/nsup)
 send
 EOF
   )
-  rm nsup
+  rm /tmp/cme-dnssec-monitor/nsup
   rndc -k "${CONF_PATH}/rndc.${view}.key" -c "${CONF_PATH}/rndc.${view}.conf" notify ${PARENT_DOMAIN} in ${view}
   rndc -k "${CONF_PATH}/rndc.${view}.key" -c "${CONF_PATH}/rndc.${view}.conf" notify ${DOMAIN} in ${view}
 done
