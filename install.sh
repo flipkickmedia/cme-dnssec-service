9d19a1b2529f9b920c6f4fb36aa78e47f491b2ff
Tom Shaw <tom@flipkick.media>
2022-06-16 18:45:31 +0100

    #8 added check for directory existance, fix perms path

--- install.sh
+++ install.sh
@@ -2,7 +2,16 @@
 # installer for cme-dnssec-monitor / external-cds-monitor
 
 # get scripts
-sudo git clone ssh://git.flipkick.media/cme/cme-bind-dnssec /usr/local/sbin/cme-bind-dnssec
+if [[ -d /usr/local/sbin/cme-bind-dnssec ]]; then
+  sudo git clone ssh://git.flipkick.media/cme/cme-bind-dnssec /usr/local/sbin/cme-bind-dnssec
+else
+  (
+    cd /usr/local/sbin/cme-bind-dnssec
+    sudo git reset --hard
+    sudo git pull
+  )
+fi
+
 sudo cp /usr/local/sbin/cme-bind-dnssec/cme-dnssec-monitor.service /etc/systemd/system/
 sudo chown root:root /etc/systemd/system/cme-dnssec-monitor.service
 
@@ -12,11 +21,11 @@ sudo chown root:root /etc/systemd/system/cme-external-cds-monitor.service
 #create config
 if [[ ! -d /etc/cme && ! -f /etc/cme/dnsset-monitor.env ]]; then
   sudo mkdir /etc/cme
-  cp cme-dnssec-monitor.env /etc/cme/
+  cp dnssec-monitor.env /etc/cme/
 fi
 chown root:root -R /etc/cme
 chmod 700 /etc/cme
-chmod 600 /etc/cme/cme-dnssec-monitor.env
+chmod 600 /etc/cme/dnssec-monitor.env
 
 #setup service
 sudo systemctl enable cme-dnssec-monitor.service
