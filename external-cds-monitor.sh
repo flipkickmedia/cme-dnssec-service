54d966b200bb51018c58cc466623d38e75d78dae
Tom Shaw <tom@flipkick.media>
2022-06-16 18:42:24 +0100

    #8 added service, renaming, written WIP service

---
+++ external-cds-monitor.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# run cme-external-cds-monitor.sh
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+if [[ ${CME_DNSSEC_MONITOR_DEBUG=notloaded} == "notloaded" ]]; then
+  . ${DIR}/cme-dnssec-monitor.env
+fi
+. ${DIR}/lib.sh
+
+# create some common time offsets
+function create_time_offsets() {
+  # $1 is the current epoch
+  DATE_10M=$(($1 + 600))
+  DATE_60M=$(($1 + 3600))
+  DATE_1D=$(($1 + 86400))
+}
+
+# external domains monitoring - check every 30mins for DS record updates
+while (true); do
+  while IFS= read -r domain; do
+    log "external CDS check:$domain"
+    touch "${DSPROCESS_PATH}/external-cds-$domain"
+    dig +short $domain NS | sort | tee ${DSPROCESS_PATH}/external-cds-$domain |
+      while IFS= read -r ns_server; do
+        dig +short $ns_server NS | sort | tee ${DSPROCESS_PATH}/external-cds-$domain-NS-A >/dev/null
+        ns_server=$(tail -n 1 "${DSPROCESS_PATH}/external-cds-$domain-NS-A")
+        echo $ns_server
+      done
+
+  done <"$EXTERNAL_DOMAINS_LIST"
+  sleep $((600))
+done
--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-# installer for cme-dnssec-monitor.sh
+# installer for cme-dnssec-monitor / external-cds-monitor
 
 # get scripts
 sudo git clone ssh://git.flipkick.media/cme/cme-bind-dnssec /usr/local/sbin/cme-bind-dnssec
 sudo cp /usr/local/sbin/cme-bind-dnssec/cme-dnssec-monitor.service /etc/systemd/system/
 sudo chown root:root /etc/systemd/system/cme-dnssec-monitor.service
 
+sudo cp /usr/local/sbin/cme-bind-dnssec/cme-external-cds-monitor.service /etc/systemd/system/
+sudo chown root:root /etc/systemd/system/cme-external-cds-monitor.service
+
 #create config
-if [[ ! -d /etc/cme && ! -f /etc/cme/cme-dnsset-monitor.env ]]; then
+if [[ ! -d /etc/cme && ! -f /etc/cme/dnsset-monitor.env ]]; then
   sudo mkdir /etc/cme
   cp cme-dnssec-monitor.env /etc/cme/
 fi
@@ -16,6 +19,9 @@ chmod 700 /etc/cme
 chmod 600 /etc/cme/cme-dnssec-monitor.env
 
 #setup service
-sudo systemctl enable bind-dnssec-monitor.service
-sudo systemctl start bind-dnssec-monitor.service
+sudo systemctl enable cme-dnssec-monitor.service
+sudo systemctl enable cme-external-cds-monitor.service
+sudo systemctl start cme-dnssec-monitor.service
+sudo systemctl start cme-external-cds-monitor.service
 sudo systemctl restart named
+return 0
